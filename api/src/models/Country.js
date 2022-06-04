1524df92cc5ebd49cb29f34b61c1279153541968
cmacha2 <cristiancmg127@gmail.com>
2022-06-04 09:18:37 -0400

    Creado el Modelo de Country

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,39 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    id:{
+      type:DataTypes.STRING(3),
+      allowNull:false,
+      primaryKey:true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    img: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    continent:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    capital:{
+      type:DataTypes.STRING,
+      allowNull:false,
+    },
+    subregion:{
+      types:DataTypes.STRING,
+    },
+    area:{
+      type:DataTypes.STRING,
+    },
+    population:{
+      type:DataTypes.INTEGER
+    }
+  },
+  {
+    timestamps:false
+  }
+  );
 };
