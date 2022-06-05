cdf1e81c92bdc62e70dcfd62ba873a28fcfeb6ea
cmacha2 <cristiancmg127@gmail.com>
2022-06-05 15:28:16 -0400

    finish the routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
+const { erroresRoute } = require('./erroresRoute');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const {getCountries} = require('./getCountries');
 const { getCountriesId } = require('./getCountriesId');
+const { getMatchCountries } = require('./getMatchCountries');
 const { postActivities } = require('./postActivities');
 
 const router = Router();
@@ -11,7 +13,9 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.get('/countries',getCountries);
+router.get('/countries',getMatchCountries)
 router.get('/countries/:id', getCountriesId)
 router.post('/activities', postActivities)
+router.use(erroresRoute)
 
 module.exports = router;
