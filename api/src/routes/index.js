70a09a845062cf88da63cac74f31c71561cf07d4
cmacha2 <cristiancmg127@gmail.com>
2022-06-05 22:01:45 -0400

    Backend finished

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,11 +1,9 @@
 const { Router } = require('express');
-const { erroresRoute } = require('./erroresRoute');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-const {getCountries} = require('./getCountries');
-const { getCountriesId } = require('./getCountriesId');
-const { getMatchCountries } = require('./getMatchCountries');
-const { postActivities } = require('./postActivities');
+const {getCountries} = require('../controllers/getCountries');
+const { getCountriesId } = require('../controllers/getCountriesId');
+const { postActivities } = require('../controllers/postActivities');
 
 const router = Router();
 
@@ -13,9 +11,8 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.get('/countries',getCountries);
-router.get('/countries',getMatchCountries)
 router.get('/countries/:id', getCountriesId)
 router.post('/activities', postActivities)
-router.use(erroresRoute)
+
 
 module.exports = router;
